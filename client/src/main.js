a48ebfdf9c8902349238d2e428ae1a79c53aa7d7
Li Da <83760906+querenguoyanshen@users.noreply.github.com>
2026-06-19 11:05:21 +0800

    feat: implement intent-driven policy demo

---
+++ client/src/main.js
@@ -0,0 +1,1 @@
+import{createApp}from'vue';import ElementPlus from'element-plus';import'element-plus/dist/index.css';import*as Icons from'@element-plus/icons-vue';import App from'./App.vue';import router from'./router';import'./style.css';const app=createApp(App);Object.entries(Icons).forEach(([k,v])=>app.component(k,v));app.use(ElementPlus).use(router).mount('#app');
